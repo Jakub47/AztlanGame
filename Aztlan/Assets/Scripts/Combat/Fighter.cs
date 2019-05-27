e4145321554890e517560d061b40e5fe47249962
Jakub47 <jakub7447@gmail.com>
2019-05-27 10:16:58 +0200

    Taking Damage based on event in animation

--- Aztlan/Assets/Scripts/Combat/Fighter.cs
+++ Aztlan/Assets/Scripts/Combat/Fighter.cs
@@ -10,7 +10,7 @@ namespace Aztlan.Combat
     {
         [SerializeField] float weaponRange = 2f;
         [SerializeField] float timeBetweenAttack = 1f;
-
+        [SerializeField] float weaponDamage = 5f;
 
         Transform target;
         float timeSinceLastAttack = 0;
@@ -36,6 +36,7 @@ namespace Aztlan.Combat
         {
             if(timeSinceLastAttack >= timeBetweenAttack)
             {
+                //Thhis will trigger HIT() EVENT;
                 GetComponent<Animator>().SetTrigger("attack");
                 timeSinceLastAttack = 0.0f;
             }
@@ -60,7 +61,8 @@ namespace Aztlan.Combat
         // Animation Event
         void Hit()
         {
-
+            Health healthComponent = target.GetComponent<Health>();
+            healthComponent.TakeDamage(weaponDamage);
         }
     }
 
---
+++ Aztlan/Assets/Scripts/Combat/Health.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Aztlan.Combat
+{
+    public class Health : MonoBehaviour
+    {
+        //change for current and max health;
+        [SerializeField] float health = 100f;
+
+        public void TakeDamage(float damage)
+        {
+            health = Mathf.Max(health - damage, 0);
+            print(health);
+        }
+    }
+}
