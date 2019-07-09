27a299f4c1f49b52f65685c830507ef6f94d7616
Jakub47 <jakub7447@gmail.com>
2019-07-09 23:44:31 +0200

    Gizmos (Visualing)

---
+++ Aztlan/Assets/Scripts/Control/PatrolPath.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Aztlan.Control
+{
+    public class PatrolPath : MonoBehaviour
+    {
+        const float gizmosRadius = 0.3f;
+        private void OnDrawGizmos()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                int j = GetNextIndex(i);
+                Gizmos.DrawSphere(GetWaypoint(i), gizmosRadius);
+                Gizmos.DrawLine(GetWaypoint(i), GetWaypoint(j));
+            }
+        }
+
+        private  int GetNextIndex(int i)
+        {
+            if (i + 1 == transform.childCount)
+                return 0;
+            return i + 1;
+        }
+
+        private Vector3 GetWaypoint(int i)
+        {
+            return transform.GetChild(i).position;
+        }
+    }
+}
